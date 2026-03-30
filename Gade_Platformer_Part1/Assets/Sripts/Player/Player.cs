662c1f7fc860ec7c5ee371293be3d0ecc2745680
harrietaleck <harrietmaleck@gmail.com>
2026-03-31 00:05:14 +0200

    add reusable player and collectable prefabs with HUD integration
    I implement Player movement/jump controller setup.
    Create ThermalStone collectable prefab with trigger pickup logic
    Hook pickups into GameManager to update score and thermal stone count
    And organized prefab structure for reuse across scenes

--- Gade_Platformer_Part1/Assets/Sripts/Player/Player.cs
+++ Gade_Platformer_Part1/Assets/Sripts/Player/Player.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
 
+// Basic 3D character for platform level, player Drop on a CharacterController + tag Player.
+
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement")]
+    public float moveSpeed = 6f;
+    public float jumpHeight = 1.2f;
+    public float gravity = -30f;
+
+    private CharacterController controller;
+    private float verticalVelocity;
+
+    private void Awake()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 input = new Vector3(h, 0f, z);
+
+        if (input.sqrMagnitude > 1f)
+            input.Normalize();
+
+        Vector3 move = input * moveSpeed;
+
+        if (controller.isGrounded && verticalVelocity < 0f)
+            verticalVelocity = -2f;
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+            verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        verticalVelocity += gravity * Time.deltaTime;
+        move.y = verticalVelocity;
+
+        controller.Move(move * Time.deltaTime);
     }
 }
+//CharacterController movement: WASD to move on the ground, Space to jump (Unity Jump input)
