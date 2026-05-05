2f000f32114a39db37009d9097521b697836a7fa
BelindaMtabane <164875190+BelindaMtabane@users.noreply.github.com>
2026-05-05 14:04:00 +0200

    Update
    the waypoints work with the nav mesh and linked adt for the enemies

--- Gade_Platformer_Part1/Assets/Scripts/Enemies/Patrol.cs
+++ Gade_Platformer_Part1/Assets/Scripts/Enemies/Patrol.cs
@@ -1,23 +1,54 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Patrol : MonoBehaviour
 {
-    public Transform[] patrolPoints;
+    public List<Transform> patrolPoints;
+
+    private LinkedList<Transform> patrolPath = new LinkedList<Transform>();
+    private LinkedListNode<Transform> currentPatrolNode;
     private int currentPointIndex = 0;
 
-    public Transform GetNextPoint()
+    private NavMeshAgent agent;
+    public bool isAttacking = false;
+    private void Start()
     {
-        //Return the nect postion point and increment the index
-        if (patrolPoints.Length == 0) return null;
-        Transform nextPoint = patrolPoints[currentPointIndex];
-        currentPointIndex = (currentPointIndex + 1) % patrolPoints.Length; //Loop back to the first point
-        return nextPoint;
-    }
-    public Transform GetCurrentPoint()
-    {
-        //Return the current position point without incrementing the index
-        if (patrolPoints.Length == 0) return null;
-        return patrolPoints[currentPointIndex];
-    }
+        agent = GetComponent<NavMeshAgent>();
 
+        //Create a for loop to convert the list into a linked list
+        for (int i = 0; i < patrolPoints.Count; i++)
+        {
+            patrolPath.AddLast(patrolPoints[i]);
+        }
+
+        //Start at the first point of the node
+        currentPatrolNode = patrolPath.First;
+        if (currentPatrolNode != null)
+        {
+            agent.SetDestination(currentPatrolNode.Value.position);
+        }
+    }
+    private void Update()
+    {
+        if (isAttacking || currentPatrolNode == null)
+        {
+            return;
+        }
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+         {
+             //Move to the next node
+             currentPatrolNode = currentPatrolNode.Next;
+
+             //Loop back to the first node
+             if (currentPatrolNode == null)
+             {
+                 currentPatrolNode = patrolPath.First;
+             }
+
+             agent.SetDestination(currentPatrolNode.Value.position);
+         }
+    }
 }
